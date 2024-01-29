a0d5093c6b4762e86ee2e040fba115b80e4284b9
Muhammad Arshad <muhammadarshad0910@gmail.com>
2024-01-29 13:04:41 +0500

    init commit

---
+++ src/backend/windows/win_manager.js
@@ -0,0 +1,131 @@
+import { BrowserWindow, dialog, app } from "electron";
+const path = require("path");
+
+
+
+class WindowManagerFactory {
+
+    constructor() { }
+
+    static GetInstance() {
+        if (!WindowManagerFactory.instance) {
+            this.mainWindow;
+            this.splashWindow;
+            WindowManagerFactory.instance = new WindowManagerFactory();
+        }
+
+        return WindowManagerFactory.instance;
+    }
+
+    async CreateWindows() {
+        await this.CreateMainWindow()
+    }
+
+    async CreateMainWindow() {
+        if (this.mainWindow) { return; }
+
+        this.mainWindow = new BrowserWindow({
+            width: 1450,
+            height: 950,
+            // icon: this.GetIconPath(),
+            show: true,
+            webPreferences: {
+                nodeIntegration: true, //process.env.ELECTRON_NODE_INTEGRATION,
+                contextIsolation: false, //!process.env.ELECTRON_NODE_INTEGRATION,
+                enableRemoteModule: true,
+                nodeIntegrationInWorker: true,
+                webSecurity: false,
+            },
+        })
+
+        // set main menu to null (set later) 
+        // this.mainWindow.setMenu(null)
+
+        // event on the main window
+        this.mainWindow.webContents.on("did-finish-load", () => {
+            if (this.splashWindow) {
+                this.splashWindow.close();
+            }
+            this.mainWindow.show(true);
+        });
+
+        this.mainWindow.webContents.on("page-title-updated", e => {
+            e.preventDefault();
+            this.mainWindow.setTitle(this.GetAppName());
+            this.mainWindow.setIcon(this.GetIconPath());
+        });
+
+        this.mainWindow.on("closed", () => (this.mainWindow = null));
+
+        this.mainWindow.on("close", e => {
+            const userCloseChoice = this.ShowCloseChoices();
+            if (userCloseChoice === 0) {
+                if (this.sideWindow) this.sideWindow.close();
+                if (this.splashWindow) this.splashWindow.close();
+                if (this.infoWindow) this.infoWindow.close();
+            } else {
+                e.preventDefault();
+            }
+        });
+
+        // serve the app.
+        try {
+            if (process.env.WEBPACK_DEV_SERVER_URL) {
+                // Load the url of the dev server if in development mode
+                await this.mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+                if (!process.env.IS_TEST) this.mainWindow.webContents.openDevTools();
+            } else {
+                // Load the index.html when not in development
+                this.mainWindow.loadURL("app://./index.html");
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    CreateSplashWindow() {
+        if (this.splashWindow) { return;  }
+
+        this.splashWindow = new BrowserWindow({
+            // parent: this.mainWindow,
+            setAlwaysOnTop: true,
+            width: 720,
+            height: 300,
+            icon: this.GetIconPath(),
+            title: this.GetAppName(),
+            show: true,
+            skipTaskbar: true,
+            frame: false,
+            center: true,
+            transparent: true,
+        });
+
+        this.splashWindow.loadFile(
+            path.join(__static, "/splash/splash.html")
+        );
+
+        this.splashWindow.setAlwaysOnTop(true);
+
+        this.splashWindow.on("closed", () => (this.splashWindow = null));
+    }
+
+
+    GetIconPath() {
+        return path.join(__static, "icons/icon.jpg");
+    }
+
+    GetAppName() {
+        return `Student Portal - v${app.getVersion()}`;
+    }
+
+    ShowCloseChoices() {
+        return dialog.showMessageBoxSync(this.mainWindow, {
+            type: "question",
+            buttons: ["Yes", "No"],
+            title: "Warning",
+            message: "Do you really want to close the student portal?",
+        });
+    }
+}
+
+export default WindowManagerFactory;
---
+++ src/background.js
@@ -0,0 +1,75 @@
+"use strict";
+
+import { app, protocol, BrowserWindow } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+import WindowManagerFactory from "@/backend/windows/win_manager"
+const windowManagerFactory = WindowManagerFactory.GetInstance()
+
+
+class CreateApp {
+  constructor() {
+    this.InitializeProtocols()
+    this.InitializeCloseConditions()
+    this.InitializeApp()
+  }
+
+  InitializeProtocols(){
+    // Scheme must be registered before the app is ready
+    protocol.registerSchemesAsPrivileged([
+      { scheme: "app", privileges: { secure: true, standard: true } },
+    ]);
+  }
+
+  InitializeCloseConditions(){
+    // Exit cleanly on request from parent process in development mode.
+    if (isDevelopment) {
+      if (process.platform === "win32") {
+        process.on("message", (data) => {
+          if (data === "graceful-exit") {
+            app.quit();
+          }
+        });
+      } else {
+        process.on("SIGTERM", () => {
+          app.quit();
+        });
+      }
+    }
+  }
+
+  InitializeApp() {
+
+    app.on("window-all-closed", () => {
+        app.quit();
+    });
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) windowManagerFactory.CreateWindows();
+    });
+
+    app.on("ready", async () => {
+
+      windowManagerFactory.CreateSplashWindow()
+      if (isDevelopment && !process.env.IS_TEST) {
+        try {
+          await installExtension(VUEJS_DEVTOOLS);
+        } catch (e) {
+          console.error("Vue Devtools failed to install:", e.toString());
+        }
+      }
+      
+      if (!process.env.WEBPACK_DEV_SERVER_URL) {
+        createProtocol('app')
+      }
+
+      windowManagerFactory.CreateWindows();
+    });
+
+  }
+}
+
+
+new CreateApp()

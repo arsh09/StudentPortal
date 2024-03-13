177285b9c10985eed2a93936510e3e6ad327090c
Muhammad Arshad <muhammadarshad0910@gmail.com>
2024-03-13 03:50:16 +0000

    Added forms with surveyjs

---
+++ src/components/Forms/forms/FormNewStudent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<Survey :survey="survey" />
+	</div>
+</template>
+
+
+<script>
+// import { Survey , Model} from "survey-vue-ui";
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core';
+import { Survey } from 'survey-vue-ui';
+import { newStudentFormJson } from "@/components/Forms/json/new_student.js";
+
+export default {
+	name: "FormNewStudent",
+	components: {
+		Survey,
+	},
+	data() {
+		const survey = new Model(newStudentFormJson);
+		return {
+			survey,
+		};
+	},
+};
+</script>

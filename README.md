4520492a419e1893cb911fff3ad355a4ebb7c058
Muhammad Arshad <muhammadarshad0910@gmail.com>
2024-08-27 04:02:11 +0100

    Fix the build, should work with release command

--- README.md
+++ README.md
@@ -1,24 +1,12 @@
-# student-portal
+# Student Portal 
 
-## Project setup
-```
-npm install
-```
+A very simple student portal for a government college in Pakistan. This is my way of giving back to the society. 
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Project setup and build
 
-### Lints and fixes files
-```
-npm run lint
-```
+1) Install NodeJS and make sure it is in the environment path. 
+2) Git clone this repo 
+3) Run `npm install` and then `npm run electron:serve`
+4) To build a new release, run `npm run release`
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,68 @@ module.exports = defineConfig({
     electronBuilder: {
       externals: [ "better-sqlite3"],
       preload: 'src/preload.js',
+
+      nodeIntegration: true,
+      __dirname: true,
+      builderOptions : {
+        asar : false,  
+        appId: "com.buildingbugs.software.student.portal",
+        productName: "Student Portal",
+
+        publish: [
+          {
+            provider: "github",
+            protocol: "https",
+            private: false,
+            owner: "arsh09",
+            repo: "StudentPortal",
+          },
+        ],
+
+        win: {
+          target: ["nsis"], 
+        },
+        portable : {
+          requestExecutionLevel: "user"
+        },
+
+        nsis: {
+          oneClick: false,
+          perMachine: false,
+          allowToChangeInstallationDirectory: false,
+        },
+
+        linux: {
+          target: ["deb", "rpm", "snap", "AppImage"],
+          category: "app.public.app-category.healthcare-fitness",
+        },
+
+        mac: {
+          category: "public.app-category.healthcare-fitness",
+          gatekeeperAssess: false,
+          hardenedRuntime: true,
+          type : "distribution", 
+          target : ["dmg", "pkg", "zip"]
+        },
+
+        dmg: {
+          contents: [
+            { x: 130, y: 210 },
+            { x: 410, y: 220, type: "link", path: "/Applications" },
+          ],
+          sign: true,
+        },
+
+        pkg: {
+          installLocation : "/Applications",
+          allowAnywhere: false,
+          allowCurrentUserHome: true,
+          allowRootDirectory: true,
+          isVersionChecked: true,
+          isRelocatable: false,
+          overwriteAction: "upgrade"
+        },
+      }
     }
   }
 
